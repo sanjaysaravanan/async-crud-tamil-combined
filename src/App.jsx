5a42450d2775688497964bcd66d927aa2c3764db
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-04-21 15:53:34 +0530

    added / for the paths

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<h1>Home Page</h1>} />
-          <Route path="products" element={<h2>Products</h2>} />
-          <Route path="products/:prodId" element={<h2>Product Page</h2>} />
-          <Route path="about-us" element={<h1>About Us Page</h1>} />
+          <Route path="/products" element={<h2>Products</h2>} />
+          <Route path="/products/:prodId" element={<h2>Product Page</h2>} />
+          <Route path="/about-us" element={<h1>About Us Page</h1>} />
           <Route path="*" element={<DefaultElement />} />
         </Route>
       </Routes>
